9630da92eb9211a5a25b3ac613f4a27e4a6842e8
ivan <ivancoto03@gmail.com>
2025-09-30 11:05:24 +0200

    feat: Created function to center the windows on the user screen

---
+++ src/utils/window_utils.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+def center_window(window : tk.Tk | tk.Toplevel):
+    window.update_idletasks()
+
+    width = window.winfo_width()
+    height = window.winfo_height()
+
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    # Calculate center point to place the window
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+
+    window.geometry(f"{width}x{height}+{x}+{y}")
